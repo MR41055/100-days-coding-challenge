c0e8fc5dba6a1e4192c7f1098f43aade1a5b08d5
MR41055 <93832049+MR41055@users.noreply.github.com>
2022-09-22 21:19:55 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,9 @@
+public class area{
+    public static void main(String[]args){
+        double a,pi,r;
+        r=10.8f;
+        pi=3.14f;
+        a=pi*r*r;
+             System.out.println("This is area"+a);
+        }
+}
