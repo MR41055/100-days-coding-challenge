4cabfa7eba17a5d6d43ee6a8ab158d04423bc331
MR41055 <93832049+MR41055@users.noreply.github.com>
2022-09-22 21:21:37 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,13 @@
+public class conditional{
+    public static void main(String[]args){
+        int x,y;
+        
+        x=10;
+        y=20;
+        if(x<y)
+            System.out.println("x<y");
+           x=x*2;
+            if(x==y)
+                 System.out.println("X is equal to y");
+                }
+}
